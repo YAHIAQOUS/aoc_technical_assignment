015b80ecd8bb6a78385e4f865362b985b2dfe102
YAHIAQOUS <yahiaqous@gmail.com>
2022-06-08 16:45:07 +0300

    FrontEnd Initial Pages Created with Header & Footer

---
+++ FrontEnd/src/components/Footer.js
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top'>
+      <div className='nav col-md-4 justify-content-center list-unstyled d-flex'>
+        <a href='/' className='text-muted text-decoration-none lh-1'>
+          <span className='text-muted'>&copy; 2022 Online Store</span>
+        </a>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ FrontEnd/src/main_pages/YourStore.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from 'react';
+
+const YourStore = () => {
+  return (
+    <>
+      <h1>Your Store Page</h1>
+    </>
+  );
+};
+export default YourStore;
